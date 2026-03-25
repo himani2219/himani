ca44bca46cbb6407d0786f3667f9212e65d2be18
Himani <himanipanchal3915@gmail.com>
2026-03-25 22:47:22 +0530

    Sidebar component created

---
+++ components/MainPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <div className='w-5/6'>MainPage</div>
+  )
+}
+
+export default MainPage
---
+++ components/Sidebar.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const Sidebar = () => {
+    const links = [
+        {
+            label:'Home',
+            icon:'$',
+            linkto: '#'
+        },
+        {
+            label:'Experience',
+            icon:'$',
+            linkto: '#'
+        },
+        {
+            label:'Projects',
+            icon:'$',
+            linkto: '#'
+        },
+        {
+            label:'Skills',
+            icon:'$',
+            linkto: '#'
+        }
+    ]
+    const socials = [
+        {
+            label:'Email',
+            icon:'$',
+            linkto: '#'
+        },
+        {
+            label:'Github',
+            icon:'$',
+            linkto: '#'
+        },
+        {
+            label:'LinkedIn',
+            icon:'$',
+            linkto: '#'
+        },
+    ]
+  return (
+    <aside className='w-1/6 bg-black h-screen'>
+        <div className="flex flex-col justify-between h-full px-5 py-8">
+            <div className="flex flex-col gap-2">
+                <h2 className='text-2xl tracking-wide font-extrabold'>Himani Panchal</h2>
+                <p className='text-zinc-500 text-sm'>Software Development Engineer</p>
+                <div className="flex flex-col w-full gap-1 mt-5">
+                    {links.map((link, i)=>(
+                        <a key={i} href={link.linkto} className='flex gap-2 p-2 active:bg-zinc-700 rounded-sm hover:ml-2 transition-all duration-200'>
+                            <span>{link.icon}</span>
+                            <span>{link.label}</span>
+                        </a>
+                    ))}
+                </div>
+            </div>
+            <div className="flex flex-col gap-2 text-zinc-500 text-sm">
+                {socials.map((link, i)=>(
+                        <a key={i} href={link.linkto} className='flex gap-2'>
+                            <span>{link.icon}</span>
+                            <span>{link.label}</span>
+                        </a>
+                    ))}
+            </div>
+        </div>
+    </aside>
+  )
+}
+
+export default Sidebar
